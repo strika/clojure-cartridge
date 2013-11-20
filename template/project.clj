6c5b78d5e46be29f9e0b08f1a1da5c2960798cf0
Marek Jelen <marek@jelen.biz>
2013-11-20 14:44:04 +0100

    Initial commit

---
+++ template/project.clj
@@ -0,0 +1,5 @@
+(defproject clojureapp "0.1.0-SNAPSHOT"
+    :plugins [[lein-ring "0.7.5"]]
+    :dependencies [[org.clojure/clojure "1.5.1"]
+                   [http-kit "2.1.12"]]
+    :main webapp)
